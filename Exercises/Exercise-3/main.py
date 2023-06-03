8a2088ddb35d3421bd57d2cd434589e31230dc4f
ferozqureshi <70518130+ferozqureshi@users.noreply.github.com>
2023-06-03 15:09:27 -0400

    commit for 4th question

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -2,7 +2,35 @@ import boto3
 
 
 def main():
-    # your code here
+    # Configure the AWS SDK
+
+    import boto3
+    import gzip
+
+    # Create an S3 client
+    s3 = boto3.client('s3')
+
+    # S3 bucket and keys
+    bucket_name = 'commoncrawl'
+    index_key = 'crawl-data/CC-MAIN-2022-05/wet.paths.gz'
+
+    # Download the index file
+    s3.download_file(bucket_name, index_key, 'wet.paths.gz')
+
+    # Extract the file
+    with gzip.open('wet.paths.gz', 'rt') as f:
+        # Read the first line
+        first_line = f.readline().strip()
+
+    # Download the file from the URI in the first line
+    s3.download_file(bucket_name, first_line, 'downloaded_file')
+
+    # Iterate through the lines of the downloaded file
+    with open('downloaded_file', 'r') as f:
+        for line in f:
+            print(line.strip())
+
+
     pass
 
 
--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -2,7 +2,43 @@ import boto3
 
 
 def main():
-    # your code here
+    import os
+    import json
+    import csv
+
+    def normalize_json(data: dict) -> dict:
+        # Your implementation of the `normalize_json` function here
+        new_data = dict()
+        for key, value in data.items():
+            if not isinstance(value, dict):
+                new_data[key] = value
+            else:
+                for k, v in value.items():
+                    new_data[key + "_" + k] = v
+        return new_data
+    def convert_json_to_csv(json_file_path: str, csv_file_path: str):
+        with open(json_file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+
+        flattened_data = normalize_json(json_data)
+
+        with open(csv_file_path, 'w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=flattened_data.keys())
+            writer.writeheader()
+            writer.writerow(flattened_data)
+
+    def process_folder(folder_path: str):
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                if file.endswith('.json'):
+                    json_file_path = os.path.join(root, file)
+                    csv_file_path = os.path.splitext(json_file_path)[0] + '.csv'
+                    convert_json_to_csv(json_file_path, csv_file_path)
+
+    # Example usage:
+    folder_path = 'B:\practice\daily_updates\Exercises\Exercise-4\data'  # Replace with the actual folder path
+    process_folder(folder_path)
+
     pass
 
 
